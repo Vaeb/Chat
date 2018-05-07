14d6ceeefdffc3894099b36ed6ee40f1a0d00347
Vaeb <vaebmail@gmail.com>
2018-05-07 21:15:50 +0100

    Think I fixed the css issue (+file names)

--- react-client/src/components/app.js
+++ react-client/src/components/app.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { browserHistory } from 'react-router';
-import HomePage from './pages/homePage.js';
-import NavBar from './headerComponent/navBar.js';
-import LeftPanel from './leftPanel/leftPanel.js';
+import HomePage from './pages/HomePage.js';
+import NavBar from './headerComponent/NavBar.js';
+import LeftPanel from './leftPanel/LeftPanel.js';
 
 class App extends Component {
     render() {
