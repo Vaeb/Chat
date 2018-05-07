0d2233a0444e2b2f5ddb071b662bad06572c607a
Vaeb <vaebmail@gmail.com>
2018-05-07 21:23:22 +0100

    navBar dir, not header

--- react-client/src/components/App.js
+++ react-client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './pages/HomePage.js';
-import NavBar from './headerComponent/NavBar.js';
+import NavBar from './navBar/NavBar.js';
 import LeftPanel from './leftPanel/LeftPanel.js';
 
 class App extends Component {
