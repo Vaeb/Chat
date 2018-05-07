570e4cd371cbd274eb2d1bd68cd0e3801e74dce1
Vaeb <vaebmail@gmail.com>
2018-05-07 21:20:40 +0100

    Rename

---
+++ react-client/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { browserHistory } from 'react-router';
+import HomePage from './pages/HomePage.js';
+import NavBar from './headerComponent/NavBar.js';
+import LeftPanel from './leftPanel/LeftPanel.js';
+
+class App extends Component {
+    render() {
+        return (
+            <div class="max">
+                <Router>
+                    <div class="max">
+                        <LeftPanel />
+                        <Route name="home" exact path="/" component={HomePage} />
+                    </div>
+                </Router>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ react-client/src/components/leftPanel/LeftPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class LeftPanel extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default LeftPanel;
