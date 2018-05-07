e1677fceba61356c2026549f2325148f91a8208c
Vaeb <vaebmail@gmail.com>
2018-05-07 22:38:39 +0100

    Inline block

--- react-client/src/components/pages/HomePage.js
+++ react-client/src/components/pages/HomePage.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render() {
         return (
-            <div>
-                This is the HomePage Route, which will be loaded when the user accesses the home page. The side bar to the left exists across pages, and as such it is separate to the existence of the HomePage route.
+            <div class="HomePage">
+                This is the HomePage Route, which will be loaded when the user accesses the home page.
+                <br />
+                The side bar to the left exists across pages, and as such it is separate to the existence of the HomePage route.
             </div>
         )
     }
