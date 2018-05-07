ef28112db1218ca2d5da5766771a26599c6550a2
Vaeb <vaebmail@gmail.com>
2018-05-07 22:41:02 +0100

    Test msg

--- react-client/src/components/pages/HomePage.js
+++ react-client/src/components/pages/HomePage.js
@@ -4,9 +4,9 @@ class HomePage extends Component {
     render() {
         return (
             <div class="HomePage">
-                This is the HomePage Route, which will be loaded when the user accesses the home page.
+                This is the HomePage Route, which will be loaded when home page is opened.
                 <br />
-                The side bar to the left exists across pages, and as such it is separate to the existence of the HomePage route.
+                The side bar to the left exists across page-routes, and as such it is separate to the existence of the HomePage component.
             </div>
         )
     }
