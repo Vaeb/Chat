cc49d7439b678e2b65c1b5cd0995ada81f62b995
Vaeb <vaebmail@gmail.com>
2018-05-08 21:52:25 +0100

    Styling

--- react-client/src/components/pages/HomePage.js
+++ react-client/src/components/pages/HomePage.js
@@ -4,9 +4,7 @@ class HomePage extends Component {
     render() {
         return (
             <div class="HomePage">
-                This is the HomePage Route, which will be loaded when home page is opened.
-                <br />
-                The side bar to the left exists across page-routes, and as such it is separate to the existence of the HomePage component.
+                Vashta chat - In development.
             </div>
         )
     }
