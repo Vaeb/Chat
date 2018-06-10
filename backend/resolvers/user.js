3da6113b84442c681e963480cfac4438753c2632
Vaeb <vaebmail@gmail.com>
2018-06-10 19:30:10 +0100

    User registration graphql

--- backend/resolvers/user.js
+++ backend/resolvers/user.js
@@ -1,9 +1,20 @@
+import bcrypt from 'bcrypt';
+
 export default {
     Query: {
         getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
         allUsers: (parent, args, { models }) => models.User.findAll(),
     },
     Mutation: {
-        createUser: (parent, args, { models }) => models.User.create(args),
+        register: async (parent, { password, ...otherArgs }, { models }) => {
+            try {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+                return user.dataValues.id;
+            } catch (err) {
+                console.log(err);
+                return -1;
+            }
+        },
     },
 };
